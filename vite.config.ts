d2fb5bc4ff57075d002bc22aef86757554fe912b
[dyad] <git@dyad.sh>
2025-11-19 16:35:34 -0300

    [dyad] add Dyad component tagger

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { mochaPlugins } from "@getmocha/vite-plugins";
+import dyadComponentTagger from '@dyad-sh/react-vite-component-tagger';
 
 export default defineConfig({
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  plugins: [...mochaPlugins(process.env as any), react()],
+  plugins: [dyadComponentTagger(), ...mochaPlugins(process.env as any), react()],
   server: {
     allowedHosts: true,
   },
