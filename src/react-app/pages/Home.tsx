5682fb3c0cd4bb44bdf76816044a67db05751901
[dyad] <git@dyad.sh>
2025-11-19 21:40:53 -0300

    Updated src/react-app/pages/Home.tsx

--- src/react-app/pages/Home.tsx
+++ src/react-app/pages/Home.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       </div>
 
       {/* Header */}
-      <header className="fixed top-0 left-0 right-0 z-50 py-2 px-3 bg-transparent">
+      <header className="top-0 left-0 right-0 z-50 py-2 px-3 bg-transparent">
         <div className="max-w-7xl mx-auto flex justify-between items-center gap-2">
           <img
             src="logo.png"
